50f558249b99320232afc9276f2d79ca637601fa
Pluton <alexandre.fenoul@ynov.com>
2020-11-01 22:41:14 +0100

    Redux test

--- src/core/layout/header/head/Head.js
+++ src/core/layout/header/head/Head.js
@@ -1,18 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { } from 'react-redux';
-import { } from './headSlice.js';
+import { selectColor, changeColor } from './headSlice.js';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './Head.module.css';
+import Switch from '@material-ui/core/Switch';
 
 const logo = "logo.png"
 
 export function Head() {
 
+  const color = useSelector(selectColor);
+  const dispatch = useDispatch();
+
   return (
-    <div className={styles.head}>
-      <img className={styles.logo} src={logo} alt="Logo" />
-      <div className={styles.titleContainer}>
-        <h1 className={styles.title}>Curriculum Vitae</h1>
+    <div>
+      <div className={styles.head}>
+        <img className={styles.logo} src={logo} alt="Logo" />
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>Curriculum Vitae</h1>
+        </div>
+        <div className={styles.switch}>
+          <Switch
+            checked={color}
+            onChange={dispatch(changeColor())}
+            name="checked"
+            inputProps={{ 'aria-label': 'secondary checkbox' }}
+          />
+        </div>
       </div>
     </div>
+    
   );
 }
--- src/core/layout/header/head/headSlice.js
+++ src/core/layout/header/head/headSlice.js
@@ -3,12 +3,17 @@ import { createSlice } from '@reduxjs/toolkit';
 export const headSlice = createSlice({
   name: 'head',
   initialState: {
+    color: true,
   },
   reducers: {
-    
+    changeColor: state => {
+      state.color = !state.color;
+    },
   },
 });
 
-// export const { } = headSlice.actions;
+export const { changeColor } = headSlice.actions;
+
+export const selectColor = state => state.color;
 
 export default headSlice.reducer;
